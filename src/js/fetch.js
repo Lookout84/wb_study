cdecaf12b7fac19b80d15fc78e46c31b7bb1c315
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2020-12-24 21:35:44 +0200

    update

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,29 +1,29 @@
 export default {
-  query: 'moon',
+  query: "moon",
   page: 1,
   perPage: 4,
   baseUrl: `https://api.pexels.com/v1`,
 
   getFetch() {
-    let key = '563492ad6f91700001000001c072a1eefd53424fb9ad129fda8328b6';
+    let key = "563492ad6f91700001000001390f9fee0a794c1182a72e49e0e0eae2";
     let params = `/search?query=${this.query}`;
     let url = this.baseUrl + params;
     let options = {
-      method: 'GET',
+      method: "GET",
       headers: {
         Authorization: key,
       },
     };
     return fetch(url, options)
-      .then(response => {
+      .then((response) => {
         // console.log(response);
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         // console.log(data);
         return data.photos;
       })
-      .then(result => {
+      .then((result) => {
         // console.log(result);
       });
   },
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,51 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { merge } = require('webpack-merge');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const WebpackBar = require('webpackbar');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBar = require("webpackbar");
 
-// console.log("DIRNAME", __dirname); // глобальная переменная, содержащая абсолютный путь к файлу
-const loadModeConfig = env => require(`./build-utils/${env.mode}.config`)(env);
-// экспорт объекта настроек
-module.exports = env =>
+const loadModeConfig = (env) =>
+  require(`./build-utils/${env.mode}.config.js`)(env);
+
+module.exports = (env) =>
   merge(
     {
       mode: env.mode,
-      context: path.resolve(__dirname, 'src'),
-      // 1. точка входа - откуда строить дерево зависимостей
-      entry: './index.js',
-      // 2. куда положить результирующий бандл
+      context: path.resolve(__dirname, "src"),
+
+      entry: "./index.js",
       output: {
         path: path.resolve(__dirname, "dist"),
-        filename: '[name].bundle.js',
+        filename: "[name].bundle.js",
       },
-      // 3. загрузчики (loaders)
       module: {
         rules: [
           {
-            test: /\.js$/, // регулярное выражение
-            exclude: /node_modules/, // через указ папку свойства не прогонять
-            use: ['babel-loader'],
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+          },
+          {
+            test: /\.html$/,
+            use: ["html-loader"],
           },
           {
             test: /\.(gif|png|jpe?g|svg)$/,
             use: [
               {
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
-                  name: '[path]/[name].[ext]',
-                  limit: 5000,
+                  name: "[path]/[name].[ext]",
+                  limit: false,
                 },
               },
             ],
           },
           {
-            test: /\.html$/,
-            use: ['html-loader'],
-          },
-          {
             test: /\.hbs$/,
-            use: ['handlebars-loader'],
+            use: ["handlebars-loader"],
           },
         ],
-        // плагины применяются к результирующему бандлу
       },
       plugins: [
         new CleanWebpackPlugin(),
